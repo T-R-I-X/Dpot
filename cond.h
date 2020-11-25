458c71b639ed6b473942b044fcb6118edff474f3
Trix <dtwatson443@gmail.com>
2020-11-25 16:15:30 -0600

    Changes

--- cond.h
+++
@@ -1,7 +0,0 @@
-/* The Reedoo Language Interpreter */
-/* (c) 2014 Francis McNamee. All Rights Reserved. */
-/* www.reedoo.org */
-
-
-/* Function Declarations */
-bool eval_cond(std::string cond);
--- io.h
+++
@@ -1,8 +0,0 @@
-/* The Reedoo Language Interpreter */
-/* (c) 2014 Francis McNamee. All Rights Reserved. */
-/* www.reedoo.org */
-
-
-/* Function Declarations */
-
-void doPRINT(std::string toPrint);
